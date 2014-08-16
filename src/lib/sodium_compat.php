2abbad688a32f657eac1940ddc88a3650e007111
Scott Arciszewski <scott@arciszewski.me>
2014-08-16 00:46:58 -0400

    Since @devi did all the groundwork with their Salt library, let's include it as a submodule here

--- src/lib/sodium_compat.php
+++ src/lib/sodium_compat.php
@@ -3,3 +3,11 @@
  * (optional) -- polyfills libsodium if the Sodium extension is not installed
  */
 
+if (!class_exists('Sodium')) {
+    require __DIR__."/Salt/autoload.php";
+
+    class Sodium extends Salt
+    {
+        // That'll do, pig. That'll do.
+    }
+}
